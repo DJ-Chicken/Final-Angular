78a663c8885fa343ca6502153d412d75000f5e14
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-04-19 09:49:00 +0200

    Navbar
    Imported Colors, and style
    
    lagde en uferdig Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#097a80",
+        dark: "#173c3e",
+        gray: "#24282C",
+        notGray: "#818181",
+        lightGray: "#d7d8d7",
+        notWhite: "#f7f8fa",
+      },
+    },
+  },
+  plugins: [],
+};
