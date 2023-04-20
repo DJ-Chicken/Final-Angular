c13e966c8ce07e1aa1a7b455ce7a101b778959e7
DJ Chicken <101567066+DJ-Chicken@users.noreply.github.com>
2023-04-20 11:02:01 +0200

    Staff Section
    Staff section is done, (with tabs)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,39 @@ import { FooterComponent } from './components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { StaffComponent } from './components/staff/staff.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, LandingPageComponent, StaffComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingPageComponent,
+    StaffComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatTabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSnackBarModule,
+    MatAutocompleteModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/staff/staff.component.ts
+++ src/app/components/staff/staff.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-staff',
   templateUrl: './staff.component.html',
-  styleUrls: ['./staff.component.scss']
+  styleUrls: ['./staff.component.scss'],
 })
 export class StaffComponent {
-
+  staffs = [
+    {
+      name: 'Andrey Gantimurov',
+      position: 'CTO',
+      img: '../../../assets/profile-pictures/p1.png',
+      description: '(Chief Technology Officer)',
+    },
+    {
+      name: 'Margarita Stoyanova',
+      position: 'CLO',
+      img: '../../../assets/profile-pictures/p2.png',
+      description: '(Chief Learning Officer)',
+    },
+    {
+      name: 'Alexander Sapozhnikov',
+      position: 'CAO',
+      img: '../../../assets/profile-pictures/p3.png',
+      description: '(Chief Administrator Officer)',
+    },
+    {
+      name: 'Andrey Gantimurov',
+      position: 'CTO',
+      img: '../../../assets/profile-pictures/p1.png',
+      description: '(Chief Technology Officer)',
+    },
+  ];
 }
